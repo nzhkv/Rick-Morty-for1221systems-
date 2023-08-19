704330b2e7feeec964ebf87eead355f16d3d3a97
NZhukov <nzhukov.rnd@gmail.com>
2023-08-19 22:11:09 +0400

    Initial Commit

---
+++ Rick&Morty(for1221systems)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty(for1221systems)
+//
+//  Created by Nikolay Zhukov on 19.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
